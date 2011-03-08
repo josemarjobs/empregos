2cf0f7f3ccc966a4d56e306e2d9bf2bd7c2338a2
Josemar da Costa Magalhaes <josemarmagalhaes@hotmail.com>
2011-03-08 13:37:47 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Empregos::Application.config.secret_token = 'f5ff114867a2701973924274b8e5935f59612095258ddc4a39c96d390a3f1da80af92cc27fb4c1559d924d0cd71238b6174d1c125c79b02b9f66101d5f83c27d'
